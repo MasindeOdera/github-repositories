1958ebbec9f7a2b6977a3a6832a164b689ff6af1
MasindeOdera <MasindeOdera@gmail.com>
2020-04-20 11:26:03 +0200

    Updates

---
+++ precache-manifest.025d7dfaf26e3e33717ffff2784978cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beb880cc72b75d4f565a5af2a7ed7636",
+    "url": "/github-repositories/index.html"
+  },
+  {
+    "revision": "a6ce322d05ad9ac86cb0",
+    "url": "/github-repositories/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "3d147dcebf4642bec3ee",
+    "url": "/github-repositories/static/js/2.67c7895b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github-repositories/static/js/2.67c7895b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6ce322d05ad9ac86cb0",
+    "url": "/github-repositories/static/js/main.c49b5631.chunk.js"
+  },
+  {
+    "revision": "97eb3acc49dcb0175cdc",
+    "url": "/github-repositories/static/js/runtime-main.8e1a2df8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github-repositories/static/media/logo.5d5d9eef.svg"
+  }
+]);
